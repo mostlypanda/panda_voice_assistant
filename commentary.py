89746a75734377c8a7cbdd8d13dc4025e77bff6b
Sarthak Mittal <sarthakmittal1461@gmail.com>
2020-07-29 13:48:09 +0530

    final commit to commentory.py

--- commentary.py
+++ commentary.py
@@ -1,14 +1,13 @@
-
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
-import datetime #pip install datetime
-import wikipedia #pip install wikipedia
-import webbrowser
-import os
-from selenium import webdriver
+import pyttsx3                          #pip install pyttsx3
+import speech_recognition as sr         #pip install speechRecognition
+import datetime                         #pip install datetime
+import wikipedia                        #pip install wikipedia
+import webbrowser                       #pip install webbrowser
+import os                               #pip install os
+from selenium import webdriver          #pip install selenium
 import smtplib
 import time 
-from pycricbuzz import Cricbuzz
+from pycricbuzz import Cricbuzz         #pip install pycricbuzz
 def cricCommentry():
         c = Cricbuzz()
         matches = c.matches()
@@ -31,7 +30,10 @@ def cricCommentry():
                 else:
                         a=c.commentary(match['id'])['commentary'][0]['comm']
 
+                        
 browser = webdriver.Chrome(executable_path=r'C:\path\to\chromedriver.exe') #path to chrome driver
+
+#voice methods defines
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -77,83 +79,16 @@ def takeCommand():
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
-
 if __name__ == "__main__":
     wishMe()
     while True:
     # if 1:
         query = takeCommand().lower()
-
-        # Logic for executing tasks based on query
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-
-        elif 'open youtube' in query:
-            # webbrowser.open("youtube.com")
-            print("hi")
-            browser.get('https://www.youtube.com/') 
-            speak("what do you want to search on youtube")
-            query1 = takeCommand().lower()
-            search=browser.find_element_by_id('search')
-            search.send_keys(query1) 
-            sbutton=browser.find_element_by_id('search-icon-legacy')
-            # browser.find_elements_by_xpath()
-            sbutton.click()
-            video=browser.find_elements_by_partial_link_text('')
-            print(video)
-            speak('which number you want to listen')
-            print(video.__len__())
-            query2 = takeCommand().lower()
-            video[19].click()
-            if(query2=='1'):
-                video[0].click()
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
-
-
-        elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
-
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-
-        elif 'email to me' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "sarthakmittal1461@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend . I am not able to send this email")    
-        elif 'commentary' in query.lower():
+        
+        if 'commentary' in query.lower():
              try:
                 cricCommentry()
              except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email") 
+                speak("Sorry my friend I am not able to do live commentory")
 
